39609492d60774bfa9c90eb77c3951dcc1ebc1ec
supekarabhi <supekarabhi7@gmail.com>
2023-08-02 19:53:50 +0530

    code for css updated

--- app/static/app/js/myscript.js
+++ app/static/app/js/myscript.js
@@ -82,3 +82,83 @@ $('.remove-cart').click(function() {
       }
     });
 })
+$('.navToggle').on('click', function (e) {
+  e.preventDefault();
+  $('body').toggleClass('navToggleActive');
+});
+
+
+$(window).scroll(function(){
+  if ($(this).scrollTop() > 10) {
+    $('body').addClass('fixedHeader');
+  } else {
+    $('body').removeClass('fixedHeader');
+  }
+});
+window.addEventListener("scroll", function() {
+  var headerCol = document.getElementById("navbar");
+  if (window.scrollY > 0) {
+    headerCol.classList.add("scrolled");
+  } else {
+    headerCol.classList.remove("scrolled");
+  }
+});
+
+$(document).ready(function() {
+  // Function to handle scroll event
+  function handleScroll() {
+    const scrollTop = $(window).scrollTop();
+    const headerCol = $('.headerCol');
+
+    // Add or remove 'scrolled' class based on scrolling position
+    if (scrollTop > 0) {
+      headerCol.addClass('scrolled');
+    } else {
+      headerCol.removeClass('scrolled');
+    }
+  }
+
+  // Call the function on page load
+  handleScroll();
+
+  // Call the function on scroll
+  $(window).scroll(handleScroll);
+});
+
+
+
+var swiper = new Swiper(".testimonialSwiper", {
+  navigation: {
+    nextEl: ".test-swiper-button-next",
+    prevEl: ".test-swiper-button-prev",
+  },
+});
+
+
+var swiper = new Swiper(".certificatesSlider", {
+  slidesPerView: 1,
+  spaceBetween: 16,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".cert-swiper-button-next",
+    prevEl: ".cert-swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+  },
+});
+
